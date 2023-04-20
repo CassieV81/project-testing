33782bc8a098a8fe8974d8958f497328ebaae5d5
CassieV <obicassievirtue@gmail.com>
2023-04-20 18:45:16 +0200

    refactor code

--- src/capitalize.test.js
+++ src/capitalize.test.js
@@ -1,13 +1,21 @@
 import capitalize from './capitalize'
 
-test('value should be Value', () => {
+test('"value" should be "Value"', () => {
   expect(capitalize('value')).toBe('Value');
 });
 
-test('angel should be Angel', () => {
+test('"angel" should be "Angel"', () => {
   expect(capitalize('angel')).toBe('Angel');
 });
 
-test('CAP should be CAP', () => {
-  expect(capitalize('CAP')).toBe('CAP');
+test('"ALLCAP" should be "ALLCAP"', () => {
+  expect(capitalize('ALLCAP')).toBe('ALLCAP');
+});
+
+test('"1234" should be "1234"', () => {
+  expect(capitalize('1234')).toBe('1234');
+});
+
+test('1234 should be "Not a string"', () => {
+  expect(capitalize(1234)).toBe('Not a string');
 });
